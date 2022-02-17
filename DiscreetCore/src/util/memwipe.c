3a438d621b7db2fc28323eff52976dbd86bf2bdd
Brandon Koerner <bksquared1024@gmail.com>
2022-02-17 16:59:59 -0500

    add support for x64

--- DiscreetCore/src/util/memwipe.c
+++ DiscreetCore/src/util/memwipe.c
@@ -7,7 +7,9 @@
 #endif
 #include "util/memwipe.h"
 
-#if defined(_MSC_VER)
+#if defined(_WIN64)
+#define SCARECROW
+#elif defined (_WIN32)
 #define SCARECROW \
     __asm{};
 #else
@@ -15,7 +17,7 @@
     __asm__ __volatile__("" : : "r"(ptr) : "memory");
 #endif
 
-#ifdef HAVE_MEMSET_S
+#ifdef __STDC_LIB_EXT1__
 
 void *memwipe(void *ptr, size_t n)
 {
@@ -63,10 +65,24 @@ void *memwipe(void *ptr, size_t n)
  * Commit: ad1907fe73334d6c696c8539646c21b11178f20f
  * BoringSSL (LICENSE: ISC)
  */
+
+/* the following code seems to fix the problem on x64. Thank you, Robert Seacord. */
+void* erase_from_memory(void* pointer, size_t size_data, size_t size_to_remove) {
+    if (size_to_remove > size_data) size_to_remove = size_data;
+    volatile unsigned char* p = pointer;
+    while (size_to_remove--) {
+        *p++ = 0;
+    }
+    return pointer;
+}
+
 static void memory_cleanse(void *ptr, size_t len)
 {
+#if defined(_WIN64)
+    erase_from_memory(ptr, len, len);
+#else
     memset(ptr, 0, len);
-
+#endif
     /* As best as we can tell, this is sufficient to break any optimisations that
        might try to eliminate "superfluous" memsets. If there's an easy way to
        detect memset_s, it would be better to use that. */
